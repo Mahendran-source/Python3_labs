2deaa4d84a0aa12f794b2244dd783e8f4893d1bb
Mahendran Moorthy <moorthy.mahendran@gmail.com>
2024-11-19 20:42:22 +0000

    Demo pin 2

--- Demo_Pin2.py
+++ Demo_Pin2.py
@@ -1,10 +1,11 @@
 import sys
+import getpass
 
 pin ='0123'
 limit = 4
 
 for tries in range(1, limit):
-    supplied_pin = input('Enter your PIN:')
+    supplied_pin = getpass.getpass('Enter your PIN:')
     if supplied_pin == pin:
         print("Valid PIN")
         print("...and after only",tries,"attempts")
